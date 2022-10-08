5a6f9f7add8e136fbb57f9b37481acce71131041
gcancer <wldyd0721@naver.com>
2022-10-08 16:11:10 +0900

    fix: Drawing House
    1. fillRect로 원하는 좌표에 도형을 배치하고, 라인을 그린 후 색을 채운다.

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그릴 때 사용
 canvas.width = 800;
 canvas.height = 800;
 
-// Rectangle, 
-// rect 또한 단축함수
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 2;
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
 ctx.fill();
