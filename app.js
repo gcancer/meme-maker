0a3d223272d5c4cb21bb2c65b485032fe7174214
gcancer <wldyd0721@naver.com>
2022-10-08 16:21:25 +0900

    feat: arc를 사용하여 도형 그리기
    1. radius 와 angle을 지정하여 원을 그리고, path를 분리하여 색을 지정

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그릴 때 사용
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(365, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
 
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.arc(290, 130, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(270, 110, 8, Math.PI, 2 * Math.PI);
+ctx.arc(310, 110, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
