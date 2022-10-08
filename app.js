6cf6656d8a2fffe19c2097774963e4627fedd483
gcancer <wldyd0721@naver.com>
2022-10-08 15:53:21 +0900

    fix: fillRect 분리 생성, 경로 지정
    1. fillRect는 단축함수. 이를 분리해서 사각형을 만들고 색을 채움.
    2. 경로를 지정하지 않으면 모두 같은 경로로 이어져 있음. beginPath를 호출하여 새 경로를 지정

--- app.js
+++ app.js
@@ -5,5 +5,20 @@ const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그릴 때 사용
 canvas.width = 800;
 canvas.height = 800;
 
-// Rectangle
-ctx.fillRect(50, 50, 100, 200);
+// Rectangle, 
+// fillRect는 shorcutFunction, 따라서 선을 그리고 채우는 작업을 해야 함.
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+// 2. 새 경로의 생성
+ctx.beginPath(); 
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+
+// 1. 같은 경로로 이어져 있어서 모든 ctx에 적용 됨.
+// setTimeout(() => ctx.fill(), 5000);
+ctx.fillStyle = "red"; 
+ctx.fill();
+
