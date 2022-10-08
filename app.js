44b90189233f125ad1f425ba9477aaf17f4dd709
gcancer <wldyd0721@naver.com>
2022-10-08 16:01:41 +0900

    fix(rect): 단축함수인 rect 분리, moveTo and lineTo 활용
    1. rect도 단축함수
    1-1. moveTo로 좌표를 옮기고, lineTo로 지정한 축으로 선을 그린다.

--- app.js
+++ app.js
@@ -6,19 +6,10 @@ canvas.width = 800;
 canvas.height = 800;
 
 // Rectangle, 
-// fillRect는 shorcutFunction, 따라서 선을 그리고 채우는 작업을 해야 함.
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
+// rect 또한 단축함수
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
 ctx.fill();
-
-// 2. 새 경로의 생성
-ctx.beginPath(); 
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-
-// 1. 같은 경로로 이어져 있어서 모든 ctx에 적용 됨.
-// setTimeout(() => ctx.fill(), 5000);
-ctx.fillStyle = "red"; 
-ctx.fill();
-
